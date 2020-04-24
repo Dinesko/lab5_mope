7dd703ee14763d903fde16a21d0ab56b7c87ea79
dinesko <borozenetsdenys@gmail.com>
2020-04-24 11:58:40 +0300

    time

--- main.py
+++ main.py
@@ -230,8 +230,3 @@ def main(n, m):
 
 if __name__ == '__main__':
     main(15, 6)
-
-st_time = time.time()
-for _ in range(100):
-        main(15, 6)
-print(f'\nСередній час роботи 1 проходження програми: {((time.time() - st_time) / 100)} сек')
